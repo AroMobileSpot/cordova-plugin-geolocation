4742581daeb6ead0b744e4bdbc6a3d7c42c6fcc0
Etienne Lescot <etienne.lescot@mobile-spot.com>
2019-06-11 12:39:00 +0200

    wip

--- www/android/geolocation.js
+++ www/android/geolocation.js
@@ -54,7 +54,7 @@ module.exports = {
                 error(new PositionError(PositionError.PERMISSION_DENIED, 'Illegal Access'));
             }
         };
-        exec(win, fail, 'Geolocation', 'clearWatch', []);
+        exec(win, fail, 'Geolocation', 'getPermission', []);
 
         return pluginWatchId;
     },
